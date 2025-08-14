2860a0064cad934fa141687b6da59daace42ac77
Max Dominichenko <mdominichenko@gmail.com>
2025-08-14 17:07:14 -0400

    refactor: split pages to separate loadable chunks to speedup loading and minimize memory usage

--- soht2-ui/src/App.tsx
+++ soht2-ui/src/App.tsx
@@ -1,12 +1,13 @@
 /* SOHT2 © Licensed under MIT 2025. */
 import './App.css';
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeModeProvider } from './theme';
 import type { Soht2User } from './api/soht2Api';
-import LoginPage from './pages/LoginPage';
-import AdminPage from './pages/AdminPage';
-import UserPage from './pages/UserPage';
+import CircularProgress from '@mui/material/CircularProgress';
+const LazyLoginPage = lazy(() => import('./pages/LoginPage'));
+const LazyAdminPage = lazy(() => import('./pages/AdminPage'));
+const LazyUserPage = lazy(() => import('./pages/UserPage'));
 
 export default function App() {
   const [user, setUser] = React.useState<Soht2User | null>(null);
@@ -26,9 +27,30 @@ export default function App() {
               )
             }
           />
-          <Route path="/login" element={<LoginPage onLogin={u => setUser(u)} />} />
-          <Route path="/admin" element={<AdminPage user={user} />} />
-          <Route path="/user" element={<UserPage user={user} />} />
+          <Route
+            path="/login"
+            element={
+              <Suspense fallback={<CircularProgress />}>
+                <LazyLoginPage onLogin={u => setUser(u)} />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <Suspense fallback={<CircularProgress />}>
+                <LazyAdminPage user={user} />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/user"
+            element={
+              <Suspense fallback={<CircularProgress />}>
+                <LazyUserPage user={user} />
+              </Suspense>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </ThemeModeProvider>
